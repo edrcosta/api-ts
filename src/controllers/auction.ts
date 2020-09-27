3d183f92042fcc8562d557837afdbea6ee01c9d0
Eder Costa <edercosta@Eders-iMac.local>
2020-09-26 21:06:03 -0300

    adding controllers

--- src/controllers/auction.ts
+++ src/controllers/auction.ts
@@ -2,8 +2,23 @@ import { Response } from 'express';
 
 export class AuctionController{
 
-    public async login(req : any, res : Response){
+    public async start(req : any, res : Response){
 
     }
 
+    public async create(req : any, res : Response){
+
+    }
+    
+    public async end(req : any, res : Response){
+
+    }
+
+    public async addBid(req : any, res : Response){
+
+    }
+
+    public async getOne(req : any, res : Response){
+
+    }
 }
