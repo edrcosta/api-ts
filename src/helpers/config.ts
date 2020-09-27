c235dacc93acfa57d49c7de0f65284ad5a4b4fcf
Eder Costa <edercosta@Eders-iMac.local>
2020-09-26 21:09:48 -0300

    adding config loader helper

---
+++ src/helpers/config.ts
@@ -0,0 +1,34 @@
+import * as fs from 'fs';
+import { IConfig } from '../interfaces';
+
+export class Config
+{
+    static config : IConfig = null;
+
+    static get() : IConfig
+    {
+        //singleton
+        if(!Config.config){ 
+            
+            const file = `${__dirname.replace('src/helpers', '')}config.json`;
+            const dbfile = `${__dirname.replace('src/helpers', '')}db-config.json`;
+
+            if(!fs.existsSync(file)) throw `Config file is missing please create`;
+            if(!fs.existsSync(dbfile)) throw `Config file is missing please create`;
+            
+            //require in runtime that whay changes are apply live
+            //each execution will get this setting
+            let config = require(file);
+            const database = require(dbfile);
+            
+            const db = database[config.env];
+
+            //@todo change this 
+            config.database = `${db.dialect}://${db.username}:${db.password}@${db.host}/${db.database}`;
+
+            Config.config = config;
+        }
+
+        return Config.config;
+    }
+}
