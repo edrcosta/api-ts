1ee7e498a2701b1f59a1a550d1270a24204626bb
Eder Costa <edercosta@Eders-iMac.local>
2020-09-27 23:35:56 -0300

    jwt authentication

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,15 +1,15 @@
 import { Response } from 'express';
 import { IAuthRequest } from '../interfaces';
-import { Config } from '../helpers';
+import { ConfigHelper, ResponseHelper } from '../helpers';
 import { BaseController } from './base-controller';
 
 import * as jwt from 'jsonwebtoken';
 
-export class Authentication extends BaseController
+export class AuthenticationController extends BaseController
 {
     public async login(req : IAuthRequest, res : Response){
 
-        const config = Config.get();
+        const config = ConfigHelper.get();
 
         //Mock of users
         const applicationUsers = [
@@ -25,21 +25,18 @@ export class Authentication extends BaseController
         for (const user of applicationUsers) {
             if (username == user.username && password == user.password) {
 
-                const token = jwt.sign(
-                    { 
-                        id: user.id, 
-                        username: user.username 
-                    }, 
-                    config.jwt, 
-                    { expiresIn: 129600 }
-                );
-
-                res.status(200).json(this.formatedResponse({
-                    token: token
-                }));
+                const userData = { 
+                    id: user.id, 
+                    username: user.username 
+                };
+                
+                const response = ResponseHelper.formatData({
+                    token: jwt.sign(userData, config.jwt.secret)
+                });
+                
+                res.status(200).json(response);
                 break;
-            }
-            else {
+            } else {
                 res.status(401).json({
                     token: null,
                     err: 'Username or password is incorrect'
