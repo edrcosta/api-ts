5e61576eabcd454d92e12307e1461e229de16a2b
Eder Costa <edercosta@Eders-iMac.local>
2020-09-28 01:58:08 -0300

    ajust renamed method

--- src/controllers/auction.ts
+++ src/controllers/auction.ts
@@ -1,34 +1,35 @@
 import { Response, Request } from 'express';
+import { ResponseHelper } from '../helpers';
 import { BaseController } from './'
 
 export class AuctionController extends BaseController
 {
     public async start(req: Request, res : Response){
-        res.json(this.formatedResponse({
+        res.json(ResponseHelper.formatData({
             hello: 'world'
         }))
     }
 
     public async create(req: Request, res : Response){
-        res.json(this.formatedResponse({
+        res.json(ResponseHelper.formatData({
             hello: 'world'
         }))
     }
     
     public async end(req: Request, res : Response){
-        res.json(this.formatedResponse({
+        res.json(ResponseHelper.formatData({
             hello: 'world'
         }))
     }
 
     public async addBid(req: Request, res : Response){
-        res.json(this.formatedResponse({
+        res.json(ResponseHelper.formatData({
             hello: 'world'
         }))
     }
 
     public async getOne(req: Request, res : Response){
-        res.json(this.formatedResponse({
+        res.json(ResponseHelper.formatData({
             hello: 'world'
         }))
     }
