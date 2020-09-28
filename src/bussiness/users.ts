776c25f9ece3ebc3194f4c54b5c4b288a709fb90
Eder Costa <edercosta@Eders-iMac.local>
2020-09-28 13:23:42 -0300

    adding user bussiness

---
+++ src/bussiness/users.ts
@@ -0,0 +1,7 @@
+import { Crud } from '../data';
+
+export class UsersBussiness extends Crud {
+    constructor(){
+        super('users', ['id', 'username', 'password_salt', 'password_hash']);
+    }
+}
