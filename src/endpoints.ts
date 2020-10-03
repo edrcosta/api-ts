0823ca64265e6016389de368f336d01ad2b201e3
Eder Costa <edercosta@Eders-iMac.local>
2020-10-03 17:14:06 -0300

    improve endpoints urls and methods

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,15 +1,16 @@
 import { AuctionController } from './controllers/auction'
-import { AuthenticationController } from './controllers/authentication';
+import { AuthenticationController, BidController } from './controllers';
 import { IEndpoint } from './interfaces'
 
 const auctionController = new AuctionController();
 const authenticationController = new AuthenticationController();
+const bidController = new BidController();
 
 export const EndpointList : IEndpoint[] = [
     { url: '/authentication', method: 'post', handdler: authenticationController.login, public: true },
     { url: '/auction/:id', method: 'get', handdler: auctionController.getOne, public: false },
-    { url: '/auction/:id/end', method: 'post', handdler: auctionController.end, public: false },
-    { url: '/auction/:id/start', method: 'post', handdler: auctionController.start, public: false },
-    { url: '/auction/:auctionId/bid', method: 'post', handdler: auctionController.addBid, public: false },    
+    { url: '/auction/:id/end', method: 'put', handdler: auctionController.end, public: false },
+    { url: '/auction/:id/start', method: 'put', handdler: auctionController.start, public: false },
+    { url: '/auction/:auctionId/bid', method: 'post', handdler: bidController.create, public: false },    
     { url: '/auction', method: 'post', handdler: auctionController.create, public: false },
 ];
