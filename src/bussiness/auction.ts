97d6d1be13adbdeeae8faeb55c5b33cdc1f904b0
Eder Costa <edercosta@Eders-iMac.local>
2020-10-03 16:56:57 -0300

    saving new bussiness rules

--- src/bussiness/auction.ts
+++ src/bussiness/auction.ts
@@ -3,6 +3,6 @@ import { Crud } from '../data';
 export class AuctionsBussiness extends Crud 
 {
     constructor(){
-        super('auctions', ['id', 'status', 'description', 'item', 'email', 'starting_price']);
+        super('auctions', ['id', 'status', 'description', 'item', 'email', 'starting_price', 'created', 'updated']);
     }
 }
--- src/bussiness/bid.ts
+++ src/bussiness/bid.ts
@@ -3,19 +3,23 @@ import { Crud } from '../data';
 export class BidBussiness extends Crud 
 {
     constructor(){
-        super('bids', ['id', 'email', 'amount', 'auctions_id']);
+        super('bids', ['id', 'email', 'amount', 'auctions_id', 'created', 'updated']);
     }
 
     async getWinner(auctionId: number){
 
         const bidBussiness = new BidBussiness();
         
-        return await bidBussiness.getOneWhere(
+        const winner = await bidBussiness.getOneWhere(
             { auctions_id: auctionId },
             [
                 ['amount', 'ASC']
             ]
         );
+
+        if(!winner || typeof winner[0] === 'undefined') return null;
+
+        return winner[0]
     }
 
     async countWithSameValue(bidAmount: number){
